22aa9feafac4e5659289f93093dda29791ff474c
志超 葛 <3529820494@qq.com>
2021-03-02 18:34:12 +0800

    change Program.cs

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace homework
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("next change!");
         }
     }
 }
